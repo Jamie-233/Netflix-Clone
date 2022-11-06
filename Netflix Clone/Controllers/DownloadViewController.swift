aa64832f3243a99b66f3c23c16cd1bc6ad36cec0
Jenkin <jiancunge.com@gmail.com>
2022-11-06 18:00:05 +0900

    chore: remove .xcodeproj folder

---
+++ Netflix Clone/Controllers/DownloadViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadViewController.swift
+//  Netflix Clone
+//
+//  Created by ? on 2/26/7515 ERA0.
+//
+
+import UIKit
+
+class DownloadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by ? on 2/26/7515 ERA0.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
